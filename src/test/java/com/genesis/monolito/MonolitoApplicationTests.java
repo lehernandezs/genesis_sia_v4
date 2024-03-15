16b08b03df3367615a0c04fe99ee4531ec49bf3a
Luis Eduardo <lehernandezs@ufpso.edu.co>
2024-03-15 18:59:35 -0500

    Creacion del proyecto

---
+++ src/test/java/com/genesis/monolito/MonolitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesis.monolito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
